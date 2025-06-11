64791c566ee96a8738b8f8eb456647b957d1d166
Arto Keskinen <arto.keskinen@fmi.fi>
2025-06-11 16:36:55 +0300

    STU-26508 switch order of waits - timeout should exit with failure

--- bin/oc-wait.sh
+++ bin/oc-wait.sh
@@ -10,13 +10,15 @@ shift
 
 rest=$*
 
+# wait for failure as background process - capture PID
+# wait for failure first, because both waits have the same timeout - we want ecflow-tasks to abort if timeout occurs
+oc wait --for=condition=failed job/$job $rest &
+failure_pid=$! 
+sleep 2
+
 # wait for completion as background process - capture PID
 oc wait --for=condition=complete job/$job $rest &
 completion_pid=$!
-
-# wait for failure as background process - capture PID
-oc wait --for=condition=failed job/$job $rest &
-failure_pid=$! 
 
 # capture exit code of the first subprocess to exit
 # need bash 4.3
@@ -24,18 +26,18 @@ failure_pid=$!
 #wait -n $completion_pid $failure_pid
 
 while [ 1 ]; do
-  if ! ps $completion_pid > /dev/null; then
-    echo "completion completed"
-    wait $completion_pid
-    exit_code=0
-    kill $failure_pid
-    break
-  fi
   if ! ps $failure_pid > /dev/null; then
     echo "failure completed"
     wait $failure_pid
     exit_code=1
     kill $completion_pid
+    break
+  fi
+  if ! ps $completion_pid > /dev/null; then
+    echo "completion completed"
+    wait $completion_pid
+    exit_code=0
+    kill $failure_pid
     break
   fi
   sleep 2
